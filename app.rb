c31ba919caa44be70ead642a30872b2dc2ec24e1
Will Swanson <wcs97214@gmail.com>
2018-07-20 10:56:41 -0700

    progress on pages

--- app.rb
+++ app.rb
@@ -14,19 +14,42 @@ end
 
 post('/store') do
   @store_name = params["title"]
-  @brands_list = Brand.all
   @new_store = Store.create({:title => @store_name})
+  @store = Store.find_by(title: @store_name)
   redirect '/'
 end
 
-# get('/stores/:id/edit') do
-#
-# end
+get('/stores/:id/edit') do
+  @store = Store.find(params[:id].to_i)
+  @all_brands = Brand.all
+  @brands_list = @store.brands.find_by(store_id: @store.id)
+  erb(:edit)
+end
+
+
+post('/stores/:id/brand') do
+  @store = Store.find(params[:id].to_i)
+  @brand_id = params["brands_drop"]
+  @add_brand = @store.brands.find_or_create_by({:id => @brand_id})
+  @all_brands = Brand.all
+  @brands_list = Brand.find_by(@store.id)
+  binding.pry
+  erb(:edit)
+end
+
+post('/stores/:id/brand/new') do
+  @store = Store.find(params[:id].to_i)
+  @brand_name = params["new_brand"]
+  @brand_price = params["new_price"]
+  @associate_brand = @store.brands.find_or_create_by({:name => @brand_name})
+  @associate_brand.update(price: @brand_price)
+  erb(:edit)
+end
 
 # get('/stores/:id/display') do
-#
+#@store = Store.find(params[:id].to_i)
 # end
 #
 # destroy('/stores/:id/remove') do
-#
+#@store = Store.find(params[:id].to_i)
 # end
