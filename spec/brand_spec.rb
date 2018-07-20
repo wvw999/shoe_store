7b66a777a7264e4181c1e3557c63e16cbc1e7809
Will Swanson <wcs97214@gmail.com>
2018-07-20 09:48:48 -0700

    add all brand tests passing validators

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -7,5 +7,22 @@ describe(Brand) do
       brand2 = Brand.create({:name => ""})
       expect(Brand.all).to(eq([brand1]))
     end
+
+    it('a brand can only have a length of 100 char name') do
+      brand1 = Brand.create({:name => "Shoes"})
+      brand2 = Brand.create({:name => "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"})
+      expect(Brand.all).to(eq([brand1]))
+    end
+
+    it('will upcase the first letter of name') do
+      brand1 = Brand.create({:name => "toad"})
+      expect(Brand.all.first.name).to(eq("Toad"))
+    end
+
+    it('will not allow duplicate brand names') do
+      brand1 = Brand.create({:name => "toad"})
+      brand2 = Brand.create({:name => "toad"})
+      expect(Brand.all).to(eq([brand1]))
+    end
   end
 end
