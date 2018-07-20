b0948f43f067df285851ebdb20a93a5bb3b7f9c4
Will Swanson <wcs97214@gmail.com>
2018-07-20 09:42:56 -0700

    add third passing test validates no duplicate entries

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -7,20 +7,22 @@ describe(Store) do
       store2 = Store.create({:title => ""})
       expect(Store.all).to(eq([store1]))
     end
-  end
 
-  describe('#title') do
     it('a store can only have a length of 100 char title') do
       store1 = Store.create({:title => "Shoes"})
       store2 = Store.create({:title => "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"})
       expect(Store.all).to(eq([store1]))
     end
-  end
 
-  describe('#title') do
     it('will upcase the first letter of title') do
       store1 = Store.create({:title => "frog"})
       expect(Store.all.first.title).to(eq("Frog"))
+    end
+
+    it('will not allow duplicate store names') do
+      store1 = Store.create({:title => "frog"})
+      store2 = Store.create({:title => "frog"})
+      expect(Store.all).to(eq([store1]))
     end
   end
 end
